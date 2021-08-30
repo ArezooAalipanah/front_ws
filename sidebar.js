a8968c0fe51396cc5ee478faf0cc392892278f17
Arezoo <arezoo.alip@gmail.com>
2021-08-30 15:46:47 +0430

    sidebar modification

--- sidebar.js
+++ sidebar.js
@@ -5,3 +5,25 @@ function togglesidebar() {
 
 btn.addEventListener("click", togglesidebar);
 
+const mediaQuery = matchMedia('(min-width: 768px)');
+
+function handleChanges(e) {
+    const isCollapsed = document.body.classList.contains('collapse-sidebar');
+
+    if(e.matches) {
+        // open sidebar
+        if (isCollapsed) {
+            document.body.classList.remove('collapse-sidebar');
+        }
+    } else {
+        if (!isCollapsed) {
+            document.body.classList.add('collapse-sidebar');
+        }
+    }
+}
+
+mediaQuery.addEventListener('change', handleChanges)
+
+// initial check
+handleChanges(mediaQuery)
+
